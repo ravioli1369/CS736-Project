1c084856224037e241858f861910f9308d08083e
Ravi Kumar <ravioli1369@gmail.com>
2024-04-29 23:35:40 +0000

    improve finetune to accept sys args

--- ravi/finetune.py
+++ ravi/finetune.py
@@ -1,16 +1,23 @@
 import os
+import sys
 from glob import glob
 
-models = glob("checkpoint/*0.pth")
+if len(sys.argv) < 3:
+    sys.exit()
+
+models = glob(f"checkpoint/{sys.argv[1]}/*.pth")
+models.sort()
 for i, model in enumerate(models):
     print(model.split("/")[-1], f"{i}/{len(models)}")
     num_iters = int(model.split("_")[-1].split(".")[0])
-    # if not (num_iters % 10000 == 0) | ((num_iters % 1000 == 0) & (num_iters <= 10000)):
-    #    os.system(f"rm {model}")
-    n = 30
-    path = f"checkpoint/finetune/{num_iters}+{num_iters//n}/"
+
+    try:
+        n = float(sys.argv[3])
+    except:
+        n = 2
+
+    path = f"checkpoint/{sys.argv[1]}/finetune/{num_iters}+{num_iters//n}/"
     if not os.path.exists(path + "g_final.pth"):
-        os.makedirs(path, exist_ok=True)
-        cmd = f"python run.py --data_root data/covid-chestxray-dataset/resized/ --mask_root data/covid-chestxray-dataset/resized/masks/1_hole/ --finetune --model_path {model} --num_iters {int(num_iters + num_iters//n)} --model_save_path {path}"
+        cmd = f"python run.py --data_root data/SARS-COV-2-Ct-Scan-Dataset/resized/non-COVID/ --mask_root data/SARS-COV-2-Ct-Scan-Dataset/resized/masks/{sys.argv[2]}/ --finetune --model_path {model} --num_iters {int(num_iters + num_iters//n)} --model_save_path {path}"
         print(cmd)
         os.system(cmd)
