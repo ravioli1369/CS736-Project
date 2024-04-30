6867bd3767dadb1add576d341781f85cc810fb01
Ravi Kumar <ravioli1369@gmail.com>
2024-04-30 13:59:00 +0000

    refine result creation to take in sys args

--- ravi/create-results.py
+++ ravi/create-results.py
@@ -1,9 +1,13 @@
 import os
+import sys
 from glob import glob
 
-data_root = "data/covid-chestxray-dataset/resized/"
-mask_root = data_root + "masks/4_holes/"
-model_paths = glob("checkpoint/model4/finetune/**/g_final.pth", recursive=True)
+if len(sys.argv) < 2:
+    sys.exit()
+
+model_paths = glob(f"checkpoint/{sys.argv[1]}/finetune/**/g_final.pth", recursive=True)
+data_root = "data/SARS-COV-2-Ct-Scan-Dataset/resized/non-COVID/"
+mask_root = os.path.dirname(os.path.dirname(data_root)) + f"/masks/{sys.argv[2]}/"
 for model_path in model_paths:
     test_cmd = f"python run.py --data_root {data_root} --mask_root {mask_root} --result_save_path {os.path.dirname(model_path)} --model_path {model_path} --test"
     print(test_cmd)
